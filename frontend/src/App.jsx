f9fab5722386d2a693a02518b0b1f6c5bea554dc
Daniel Gruitt <dan@thetechnologyworkshop.co.uk>
2025-07-06 17:33:50 +0100

    adds simple ui fro getting addresses (limited to 50)n as a simple demo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box, Container } from '@mui/material';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import Addresses from './pages/Addresses.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <NavBar />
+      <Container maxWidth={false} sx={{ pt: { xs: 7, sm: 8 } }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/addresses" element={<Addresses />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
